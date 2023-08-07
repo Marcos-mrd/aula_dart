8902322449c65bc7a3018f250e1562d933c46843
Marcos Rian <131389602+Marcos-mrd@users.noreply.github.com>
2023-08-07 20:18:47 -0300

    adiciona_aula01

---
+++ fundamentos/variaveis.const.dart
@@ -0,0 +1,20 @@
+void main(){
+  //variaveis em dart
+  
+  //dados texto;
+  String nome = "marcos";
+
+  //tipos numericos;
+  int numero1 =  20;
+  double numero2 = 20;
+
+  //tipos de  dado constante; 
+  final num = 20;
+
+  //tipos de dados logicos; 
+  bool nomeGrande = true ; 
+
+ //modelo de interpolação de dados em string ; 
+  print("meu nome é marcos $nome , idade de $numero2 ");
+
+}
